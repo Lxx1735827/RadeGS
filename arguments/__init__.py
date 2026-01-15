768ece4e9ae0a0cb3994785d110bd55e14140df1
Lxx1735827 <870375416@qq.com>
2026-01-15 14:41:59 +0800

    参数调整

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -75,11 +75,11 @@ class PipelineParams(ParamGroup):
 
 class OptimizationParams(ParamGroup):
     def __init__(self, parser):
-        self.iterations = 30_000
+        self.iterations = 7500
         self.position_lr_init = 0.00016
         self.position_lr_final = 0.0000016
         self.position_lr_delay_mult = 0.01
-        self.position_lr_max_steps = 30_000
+        self.position_lr_max_steps = 7500
         self.feature_lr = 0.0025
         self.opacity_lr = 0.05
         self.scaling_lr = 0.005
@@ -92,8 +92,8 @@ class OptimizationParams(ParamGroup):
         self.densification_interval = 100
         self.opacity_reset_interval = 3000
         self.densify_from_iter = 500
-        self.densify_until_iter = 15_000
-        self.regularization_from_iter = 15_000
+        self.densify_until_iter = 5000
+        self.regularization_from_iter = 5000
         # self.regularization_from_iter = 0
         self.densify_grad_threshold = 0.0002
 
