e6e45e365fedd089b5c4d700e7e30bce44a78ddd
Lxx1735827 <870375416@qq.com>
2026-03-02 14:35:50 +0800

    master

--- train.py
+++ train.py
@@ -115,7 +115,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
     progress_bar = tqdm(range(first_iter, opt.iterations), desc="Training progress")
     first_iter += 1
     for iteration in range(first_iter, opt.iterations + 1):
-        if iteration > 5000:
+        if opt.densify_from_iter < iteration < opt.densify_until_iter and iteration % opt.densification_interval == 0:
             train = True
         else:
             train = False
@@ -369,10 +369,10 @@ if __name__ == "__main__":
     parser.add_argument('--port', type=int, default=6009)
     parser.add_argument('--debug_from', type=int, default=-1)
     parser.add_argument('--detect_anomaly', action='store_true', default=False)
-    parser.add_argument("--test_iterations", nargs="+", type=int, default=[3000, 5000, 8000])
-    parser.add_argument("--save_iterations", nargs="+", type=int, default=[3000, 5000, 8000])
+    parser.add_argument("--test_iterations", nargs="+", type=int, default=[1500, 3000])
+    parser.add_argument("--save_iterations", nargs="+", type=int, default=[1500, 3000])
     parser.add_argument("--quiet", action="store_true")
-    parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[5000])
+    parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[1500])
     parser.add_argument("--start_checkpoint", type=str, default = None)
     args = parser.parse_args(sys.argv[1:])
     args.save_iterations.append(args.iterations)
