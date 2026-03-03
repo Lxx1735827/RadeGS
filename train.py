b487bb7cc789a9fd835f09fc00f13323ae5597af
Lxx1735827 <870375416@qq.com>
2026-03-03 11:34:11 +0800

    master

--- train.py
+++ train.py
@@ -93,12 +93,12 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
     trian_source_path = os.path.join(dataset.source_path, "train")
     file_count = len(os.listdir(trian_source_path))
     factor = file_count // 30 + 1
-    opt.iterations = factor * 1100
-    opt.position_lr_max_steps = factor * 1100
+    opt.iterations = factor * 1050
+    opt.position_lr_max_steps = factor * 1050
     opt.densify_until_iter = factor * 500
     opt.regularization_from_iter = factor * 500
-    testing_iterations = [factor * 500, factor * 1000, factor * 1100]
-    saving_iterations = [factor * 500, factor * 1000, factor * 1100]
+    testing_iterations = [factor * 500, factor * 1000, factor * 1050]
+    saving_iterations = [factor * 500, factor * 1000, factor * 1050]
     checkpoint_iterations = [factor * 500]
 
     gaussians.training_setup(opt)
