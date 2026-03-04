dd6a35e6355b7f8eb9f96345b901d3a7d55c0812
Lxx1735827 <870375416@qq.com>
2026-03-04 09:35:09 +0800

    master

--- train.py
+++ train.py
@@ -228,7 +228,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
             # loss = rgb_loss + depth_normal_loss * lambda_depth_normal+0.2*moge_normal_loss
             loss = rgb_loss + 0.2*moge_normal_loss
         else:
-            loss = rgb_loss + depth_normal_loss * lambda_depth_normal
+            loss = rgb_loss
         # loss = rgb_loss + depth_normal_loss * lambda_depth_normal + 0.1*pcc_depth_loss
         # loss = rgb_loss + depth_normal_loss * lambda_depth_normal + 0.05*depth_loss
         # loss = rgb_loss + depth_normal_loss * lambda_depth_normal
